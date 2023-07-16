f3a742b82cdbd24d08d31bfbec21d1a0793408fb
Ismail ZAHIR <ismailza407@gmail.com>
2023-07-16 02:44:55 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,26 +9,6 @@
  **/
 int main(void)
 {
-	// int i, j;
-
-	// for (i = 0; i <= 98; i++)
-	// {
-	// 	for (j = i + 1; j <= 99; j++)
-	// 	{
-	// 		putchar((i / 10) + '0');
-	// 		putchar((i % 10) + '0');
-	// 		putchar(' ');
-	// 		putchar((j / 10) + '0');
-	// 		putchar((j % 10) + '0');
-
-	// 		if (i == 98 && j == 99)
-	// 			continue;
-
-	// 		putchar(',');
-	// 		putchar(' ');
-	// 	}
-	// }
-
 	int i, j, k, l;
 
 	for (i = 0; i <= 9; i++)
@@ -37,7 +17,7 @@ int main(void)
 		{
 			for (k = 0; k <= 9; k++)
 			{
-				for (l = k + 1; l <= 9; l++)
+				for (l = 0; l <= 9; l++)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
