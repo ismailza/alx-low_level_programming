0cbffd17cd2089ed36923d420aa9441a5f2f5996
Ismail ZAHIR <ismailza407@gmail.com>
2023-08-02 10:51:19 -0700

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursive function that print a string
+ * @s: string
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
